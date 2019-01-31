b8d5b0b8e6779ecb6744192a6bd5180ba354e988
118351691 <118351691@BISB41VL.bis.ad.ucc.ie>
2019-01-31 14:03:46 +0000

    First Commit

---
+++ Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE>
+
+<!--
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ -->
+
+ <HTML>
+     <HEAD>
+         <Meta charset="UTF-8">
+         <title></title>
+     </HEAD>
+     <body>
+         <?php
+         echo "Hello World";
+         ?>
+     </body>
+ </HTML>
